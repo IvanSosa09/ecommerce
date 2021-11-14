436023a028835424501f8bb4ce0cd6d6d114987b
Ivan Sosa <dannielsosa31@gmail.com>
2021-11-14 17:47:47 -0300

    route mongo atlas

--- database/mongodb.js
+++ database/mongodb.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/commerce");
+// mongoose.connect("mongodb://127.0.0.1:27017/commerce");
+mongoose.connect("mongodb+srv://ivansosa:ivancoder123@clustercoder.uppwp.mongodb.net/coderdb?retryWrites=true&w=majority");
+
 mongoose.connection.on("open",()=>{
     console.log("database connected")
 })
